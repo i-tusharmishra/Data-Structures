2ac22dbd69b3716ca7252adb5ba8fc3c72ec9ce6
Tushar Mishra <tushar01mishra01@gmail.com>
2026-02-14 17:00:11 +0530

    Third Pattern Commit.

---
+++ src/Patterns/IncreasingNumberTriangle.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class IncreasingNumberTriangle {
+    public static void main(String[] args) {
+        /* This code takes a number n input from user.
+        and Prints a INCREASING NUMBER PATTERN till n in n rows.
+        Example: Input = 3
+                 1
+                 1 2
+                 1 2 3
+         */
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
